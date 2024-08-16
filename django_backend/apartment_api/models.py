f2248c5587feb3b74d62ddda6dac46502c3fa0b8
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-16 03:14:29 -0500

    Backend has been tested locally with Postman but has not been containerized

--- django_backend/apartment_api/models.py
+++ django_backend/apartment_api/models.py
@@ -4,16 +4,15 @@ from django.db import models
 class Apartment(models.Model):
     name = models.CharField(max_length=100)
     
-    description = models.TextField()
+    # description = models.TextField(blank=True, null=True)
     
-    price = models.IntegerField()
+    # price = models.IntegerField(blank=True, null=True)
     
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-        
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # address = models.CharField(max_length=100, blank=True, null=True)
+    # city = models.CharField(max_length=100, blank=True, null=True)
+    # state = models.CharField(max_length=100, blank=True, null=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
--- django_backend/apartment_api/serializers.py
+++ django_backend/apartment_api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from apartment_api.models import Apartment
+from .models import Apartment
 
 class ApartmentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Apartment
-        fields = ['name', 'description', 'price', 'address', 'city', 'state', 'created_at', 'updated_at', 'id']
-        read_only_fields = ['created_at', 'updated_at', 'id']
+        fields = ['name','id']
+        read_only_fields = ['id']
+        # fields = ['name', 'description', 'price', 'address', 'city', 'state', 'created_at', 'updated_at', 'id']
+        # read_only_fields = ['created_at', 'updated_at', 'id']

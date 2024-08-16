c71caa0b108fc38c40cf5e944b12d638fc8f7208
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-16 03:12:07 -0500

    Routers logic centralizes here

--- django_backend/ApartmentCore/urls.py
+++ django_backend/ApartmentCore/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for ApartmentCore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from apartment_api.views import ApartmentViewSet
-from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'apartment', ApartmentViewSet)
+router = DefaultRouter()
+router.register(r'apartment', ApartmentViewSet, basename='apartment')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/', include('rest_framework.urls', namespace='rest_framework'))
+    path('v1/', include(router.urls)),
 ]
+
+print("Router URLs:", router.urls)  # Keep this for debugging
+print("All URLs:", urlpatterns)  # Keep this for debugging

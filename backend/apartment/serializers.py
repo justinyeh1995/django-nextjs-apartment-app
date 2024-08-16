997fa677c257e0c97d5f51a929cc2b2d0457f4ca
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-16 01:28:02 -0500

    Remove the entire folder with a new django_backend

--- backend/apartment/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from apartment.models import Apartment
-
-class ApartmentSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Apartment
-        fields = ['name', 'description', 'price', 'address', 'city', 'state', 'created_at', 'updated_at', 'id']
-        read_only_fields = ['created_at', 'updated_at', 'id']

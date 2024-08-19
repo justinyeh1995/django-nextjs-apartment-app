10af1401a4ca4ab0298dd0c1a5a98a4f8b3c415b
justinyeh1995 <justinyeh1995@gmail.com>
2024-08-19 11:33:32 -0500

    queryset order by id

--- django_backend/apartment_api/views.py
+++ django_backend/apartment_api/views.py
@@ -4,7 +4,7 @@ from .models import Apartment
 # Create your views here.
 
 class ApartmentViewSet(viewsets.ModelViewSet):
-    queryset = Apartment.objects.all()
+    queryset = Apartment.objects.all().order_by('id')
     serializer_class = ApartmentSerializer
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     # permission_classes = [AllowAny] # allow any user to access the api at this point
